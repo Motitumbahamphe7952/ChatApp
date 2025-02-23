4742267001d4fb43f5c3de822fd64b9b320f52e2
nikhil <nikhillimbu918@gmail.com>
2025-02-23 22:44:36 +0545

    added code

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const RegisterPage = () => {
   return (
     <div className="mt-5">
-        <div className="bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4">
+      <div className="bg-white w-full max-w-sm mx-2 rounded overflow-hidden p-4">
         <h3>Welcome to chat app</h3>
 
-
         <form action="">
-            <div>
-                
-            </div>
+          <div>
+            <label htmlFor="name">Name :</label>
+            <input type="text" id="name" placeholder=" Enter your name" className="bg-slate-100 px-2 py-1 focus:bg-primary focus:text-gray-800" />
+          </div>
         </form>
-        </div>
+      </div>
     </div>
   );
 };
