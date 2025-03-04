08ddac5207ac4a3505513f172f8a368b46a44aaf
nikhil <nikhillimbu918@gmail.com>
2025-03-05 01:01:21 +0545

    add home code

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { LuUserRoundPlus } from "react-icons/lu";
+import { PiChatTeardropDots } from "react-icons/pi";
+import { LuLogOut } from "react-icons/lu";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <div className="w-full h-full">
+      <div className="bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-l py-5 text-slate-600 flex flex-col justify-between">
+        <div>
+          <NavLink
+            className={({ isActive }) =>
+              `w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded ${
+                isActive && "bg-slate-200"
+              }`
+            }
+            title=" Let's Chat"
+          >
+            <PiChatTeardropDots size={30} />
+          </NavLink>
+
+          <div
+            className="w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded"
+            title=" add friend"
+          >
+            <LuUserRoundPlus size={20} />
+          </div>
+        </div>
+        <button>
+            
+        </button>
+        <button
+          className="w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded"
+          title="logout"
+        >
+          <span>
+            <LuLogOut size={20} />
+          </span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
