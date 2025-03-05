fa6223ed55175080255606eec4799d4fe2cf4371
nikhil <nikhillimbu918@gmail.com>
2025-03-05 23:30:52 +0545

    add code

--- client/src/components/EditUserDetails.jsx
+++ client/src/components/EditUserDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const EditUserDetails = ({onClose,data}) => {
   return (
-    <div ClassName="fixed top-0 bottom-0 left-0 right-0">
+    <div className="fixed top-0 bottom-0 left-0 right-0 bg-gray-700/40 flex justify-center items-center">{/* here bg-gray-700/40 ---means 40% opacity and 700 contrast */}
         EditUserDetails
     </div>
   )
