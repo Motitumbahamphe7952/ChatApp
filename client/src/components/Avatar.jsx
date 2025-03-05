514a60688315f58ece7984107bea4992c660a395
nikhil <nikhillimbu918@gmail.com>
2025-03-05 14:57:29 +0545

    added avatar.js file

---
+++ client/src/components/Avatar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { PiUserCircleThin } from "react-icons/pi";
+
+const Avatar = ({ width = 50, height = 50 }) => {
+  const {
+    _id: userId,
+    name,
+    profilepic: imageUrl,
+  } = useSelector((state) => state.user);
+
+  let avatarName = "";
+  if (name) {
+    const splitName = name?.split(" ");
+    if (splitName.length > 1) {
+      avatarName = splitName[0][0] + splitName[1][0];
+    } else {
+      avatarName = splitName[0][0];
+    }
+  }
+  return (
+    <div className="text-slate-800 overflow-hidden rounded-full">
+      {imageUrl ? (
+        <img
+          src={imageUrl}
+          alt={name}
+          width={width}
+          height={height}
+          className="overflow-hidden rounded-full"
+        />
+      ) : name ? (
+        <div
+          style={{ width: width + "px", height: height + "px" }}
+          className="overflow-hidden rounded-full"
+        >
+          {avatarName}
+        </div>
+      ) : (
+        <PiUserCircleThin size={width} />
+      )}
+    </div>
+  );
+};
+
+export default Avatar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { backendURL } from "../constant.js";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,6 +10,7 @@ const Home = () => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
 
   console.log("redux user", user);
   const fetchUserDetails = async () => {
