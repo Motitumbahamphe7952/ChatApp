7e06acb779d0f48285c277db2188354e63e36c40
nikhil <nikhillimbu918@gmail.com>
2025-03-18 02:00:18 +0545

    add code to searchuser

--- client/src/components/UserSearchCards.jsx
+++ client/src/components/UserSearchCards.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Avatar from "./Avatar";
 
-const UserSearchCards = () => {
-  return <div>UserSearchCards</div>;
+const UserSearchCards = ({user}) => {
+  return (
+    <div className="flex items-center gap-3 p-1">
+        <div>
+            <Avatar
+            width={50}
+            height={50}
+            name={user?.name}
+            profilepic={user?.profilepic}/>
+        </div>
+        <div>
+            <div className="font-semibold">
+                {user?.name}
+            </div>
+            <p className="text-sm">{user?.email}</p>
+        </div>
+    </div>
+);
 };
 
 export default UserSearchCards;
